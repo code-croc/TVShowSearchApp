c42876bc049702f55115d4eb7683817f8f3be619
Vince <caldwell.vincent@outlook.com>
2022-04-12 15:23:24 -0500

    adds makeImages function to app.js

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const form = document.querySelector('#searchForm')
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit',async function(e){
     e.preventDefault();
-    console.log("Submitted!");
+    const searchTerm = form.elements.query.value;
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    console.log(res.data);
+    makeImages(res.data);
+    
 })
+
+
+const makeImages=(shows)=>{
+    for(let result of shows){
+        if(result.show.image){
+            const img = document.createElement("img");
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
